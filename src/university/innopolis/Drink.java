b53b029d103d07794f944e7dedc9a6cc230ed817
Ильназ Нажипович Минниахметов <66380719+Ilnaz108@users.noreply.github.com>
2020-06-28 09:52:00 +0300

    Update Drink.java

--- src/university/innopolis/Drink.java
+++ src/university/innopolis/Drink.java
@@ -1,17 +1,32 @@
 package university.innopolis;
 
 public enum Drink {
-    COFFEE(1), MILK(2), CHICORY(3), WATER(4), JUICE(5);
+    COFFEE(20, 1), MILK(25, 2), CHICORY(35, 3), WATER(30, 4), JUICE(40, 5);
 
     private String title;
+    private int cost;
+    private int num;
 
-    Drink(String title, int cost) {this.title = title;
+    Drink(String title, int cost) {
+        this.title = title;
+        this.num = ordinal();
     }
 
-    Drink(int cost) {
+    Drink(int cost, int num) {
+        this.title = name();
+        this.num = num;
+        this.cost = cost;
     }
 
-    public String getTitle(){
+    public String getTitle() {
         return title;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public int getNum() {
+        return num;
     }
 }
